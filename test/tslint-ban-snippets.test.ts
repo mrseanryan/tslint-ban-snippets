f35f4792cfeb18a528df651db56653376f82b21b
Sean Ryan <sean.ryan@mendix.com>
2018-11-28 15:31:27 +0100

    Initial commit - setup from typescript-library-starter

---
+++ test/tslint-ban-snippets.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from "../src/tslint-ban-snippets"
+
+/**
+ * Dummy test
+ */
+describe("Dummy test", () => {
+  it("works if true is truthy", () => {
+    expect(true).toBeTruthy()
+  })
+
+  it("DummyClass is instantiable", () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
