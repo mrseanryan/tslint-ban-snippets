b2a0826bcb8fddc3cb4d4c7e063c5fd271ca6a97
str <str_ie@yahoo.co.uk>
2018-11-29 23:30:39 +0100

    fix(commit.sh): Fix commit script so arrow keys can select the commit type.

--- commit.sh
+++ commit.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-npm run commit
+# note: using yarn instead of npm, as workaround for issue where arrow keys do not work on Windows:
+# https://github.com/commitizen/cz-cli/issues/10
+
+yarn run commit
